01087d1938d74da9a0e7cbc3d02c4265a36dbe79
Catherine Galkina <anais94@yandex.ru>
2016-03-25 21:40:43 +0300

    Add Main.hs

---
+++ app/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Data.Proxy
+import Servant.Server
+
+import Server
+import API
+
+main :: IO ()
+main = run 8800 app
+
+app :: Application
+app = serve (Proxy :: Proxy BooksAPI) server
+
